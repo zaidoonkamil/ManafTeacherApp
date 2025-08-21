e782767edcbaf5178c79adebe721868009336c8f
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-08-21 15:48:50 +0300

    update 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.napol.manaf_teacher"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     signingConfigs {
